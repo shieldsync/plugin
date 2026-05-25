128be282b2ff0a9bbce2c7f52255b728fa4af773
Nipuna Prabashwara <nipuna609@gmail.com>
2026-05-25 22:25:13 +0530

    feat: add initial plugin structure and database activation logic

---
+++ shield-sync.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name: ShieldSync Security
+ * Plugin URI:  https://getshieldsync.com
+ * Description: WordPress Firewall & Intrusion Prevention System
+ * Version:     1.0.0
+ * Author:      ShieldSync
+ * Author URI:  https://getshieldsync.com
+ * License:     GPL-2.0+
+ * Text Domain: shield-sync
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+define( 'SHIELD_SYNC_VERSION', '1.0.0' );
+define( 'SHIELD_SYNC_PATH', plugin_dir_path( __FILE__ ) );
+define( 'SHIELD_SYNC_URL', plugin_dir_url( __FILE__ ) );
+
+require_once SHIELD_SYNC_PATH . 'src/Activator/Activator.php';
+
+register_activation_hook( __FILE__, ['ShieldSync\Activator\Activator', 'activate'] );
+
+add_action( 'plugins_loaded', function() {
+    // Engine boots here next
+});
